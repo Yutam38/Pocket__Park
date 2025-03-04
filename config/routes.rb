7194627289815b9c0f7d88d30cadfb1289ea1e9f
Oscar Johnson <oscarj415@gmail.com>
2025-03-04 17:57:55 +0100

    parks-actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :parks, only: [:index, :new, :create]
+  resources :parks, only: [:index, :new, :create, :show]
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
