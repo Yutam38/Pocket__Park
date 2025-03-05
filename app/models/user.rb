6b0c34fde87c34af402a43f73d19f9fb35a17fb0
Dariusz Tomaszewski <dd@darekdarko.com>
2025-03-05 16:46:54 +0100

    fixed merging conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-<<<<<<< HEAD
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :bookings
-=======
-  devise  :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
   has_many :favourites
   has_many :favourite_parks, through: :favourites
->>>>>>> master
 end
