30cd6c2eaca8dd04eed3fb05ed868887364e4972
Tamari Pilia <tamaripilia13@gmail.com>
2025-03-05 16:38:51 +0100

    changed the routes for facvourites, added new controller for favourites, added index action and indext html for favourites

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+  has_many :favourites
+  has_many :favourite_parks, through: :favourites
 end
