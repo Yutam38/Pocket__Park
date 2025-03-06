200edf7bdc763aede66c09968396d0d421a64ad9
Yuta Miyanaga <three8.film@gmail.com>
2025-03-06 15:50:31 +0100

    Added timetable date

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,25 +1,26 @@
 class ParksController < ApplicationController
   before_action :set_park, only: %i[show]
-    def index
-      @parks = Park.all
-      # The `geocoded` scope filters only flats with coordinates
-      @markers = @parks.geocoded.map do |park|
-        {
-          lat: park.latitude,
-          lng: park.longitude,
-          info_window_html: render_to_string(partial: "info_window", locals: {park: park})
-        }
-      end
+  def index
+    @parks = Park.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @parks.geocoded.map do |park|
+      {
+        lat: park.latitude,
+        lng: park.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { park: park })
+      }
     end
-
+  end
 
   def show
     @timeslots = @park.timeslots.order(:start_time)
     @markers = [{
       lat: @park.latitude,
       lng: @park.longitude,
-      info_window_html: render_to_string(partial: "info_window", locals: {park: @park})
+      info_window_html: render_to_string(partial: "info_window", locals: { park: @park })
     }]
+      selected_date = Date.today
+    @timeslots = @park.timeslots.where("DATE(start_time AT TIME ZONE 'UTC') = ?", selected_date).order(:start_time)
   end
 
   # def new
