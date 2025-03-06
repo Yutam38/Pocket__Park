d2ec98d987b744b39aa6ddd7a2c1b364673b567b
Oscar Johnson <oscarj415@gmail.com>
2025-03-06 14:38:38 +0100

    updated map and assocaited card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @parks = Park.all
+      # The `geocoded` scope filters only flats with coordinates
+      @markers = @parks.geocoded.map do |park|
+        {
+          lat: park.latitude,
+          lng: park.longitude,
+          info_window_html: render_to_string(partial: "parks/info_window", locals: {park: park})
+        }
+      end
   end
 end
