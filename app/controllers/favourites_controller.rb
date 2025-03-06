d0c9ee5e10a8ceb08a3d6c67e298aba2f95049f4
Tamari Pilia <tamaripilia13@gmail.com>
2025-03-06 10:40:55 +0100

    Added create action to fav controller

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -3,4 +3,15 @@ class FavouritesController < ApplicationController
   def index
     @favourites = current_user.favourites.includes(:park)
   end
+
+  def create
+    @park = Park.find(params[:park_id])
+    @favourites = Favourites.new(user: current_user, park: @park)
+    if @favourites.save
+      redirec_to @park, notice: "Added to favourites"
+    else
+      render @park, alert: "Something went wrong"
+  end
+
+
 end
